a81bfdb80b7d36bf0dd72eab7f2e57c4a66591ce
Carissa <carissashum@hotmail.com>
2020-11-18 23:19:04 -0800

    Added javascript for the navigation bar

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
+const nav = document.querySelector('.main-nav');
+
+function myFunction(x) {
+	if (x.matches) { 
+    matchScreen = true;
+
+    nav.style.visibility = 'hidden';
+    nav.style.opacity = '0';
+	} else {
+    matchScreen = false;
+    nav.style.visibility = 'visible';
+    nav.style.opacity = '1';
+	}
+}
+
+
+window.onscroll =  function(){
+  if ((matchScreen && document.body.scrollTop > 180) || (matchScreen && document.documentElement.scrollTop > 180)) {
+    nav.style.visibility = 'visible';
+    nav.style.opacity = '1';
+  } else if (!matchScreen){
+    nav.style.visibility = 'visible';
+    nav.style.opacity = '1';
+  } else{
+    nav.style.visibility = 'hidden';
+    nav.style.opacity = '0';
+  }
+}
+
 var x = window.matchMedia("(min-width: 48rem)");
 myFunction(x); // Call listener function at run time
 x.addListener(myFunction);
-
-const nav = document.querySelector('.main-nav');
-function myFunction(x) {
-  	if (x.matches) { 
-		window.onscroll = function() {scrollNav()}
-  	} else {
-	    nav.style.display = 'block';
-  	}
-}
-
-function scrollNav() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    nav.style.display = 'block';
-  } else{
-  	nav.style.display = 'none';
-  }
-}
